a6cb7031d7e450da6a51ccd62d31e59dafc3b5c5
Bartolinnni <128548610+Bartolinnni@users.noreply.github.com>
2023-04-12 21:49:29 +0200

    Delete blog directory

--- blog/main/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class CreatePost(forms.Form):
-    name = forms.CharField(label='Name', max_length=200)
-    text = forms.CharField(widget=forms.Textarea(attrs={'rows': '50', 'cols': '150'}))
